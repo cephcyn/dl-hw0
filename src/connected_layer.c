b30c57fd41007e6472fd185c851617fe5b683053
Will Chen <30908073+WillChen789@users.noreply.github.com>
2020-10-12 20:23:51 -0700

    Forward Connected Layer

--- src/connected_layer.c
+++ src/connected_layer.c
@@ -38,7 +38,9 @@ void backward_bias(matrix delta, matrix db)
 matrix forward_connected_layer(layer l, matrix in)
 {
     // TODO: 3.1 - run the network forward
-    matrix out = make_matrix(in.rows, l.w.cols); // Going to want to change this!
+    matrix out = matmul(in, l.w);
+    forward_bias(out, l.b);
+    activate_matrix(out, l.ACTIVATION);
 
     // Saving our input and output and making a new delta matrix to hold errors
     // Probably don't change this
