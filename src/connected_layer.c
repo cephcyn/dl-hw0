b58127cc96f86f472c40d242a07a4ac1ebb20a5a
Joyce Zhou <9123137+cephcyn@users.noreply.github.com>
2020-10-15 18:17:24 -0700

    implemented update_connected_layer

--- src/connected_layer.c
+++ src/connected_layer.c
@@ -101,15 +101,19 @@ void update_connected_layer(layer l, float rate, float momentum, float decay)
 
     // For our weights we want to include weight decay:
     // l.dw = l.dw - decay * l.w
+    axpy_matrix(-1 * decay, l.w, l.dw);
 
     // Then for both weights and biases we want to apply the updates:
     // l.w = l.w + rate*l.dw
-    // l.b = l.b + rade*l.db
-
+    axpy_matrix(rate, l.dw, l.w);
+    // l.b = l.b + rate*l.db
+    axpy_matrix(rate, l.db, l.b);
 
     // Finally, we want to scale dw and db by our momentum to prepare them for the next round
     // l.dw *= momentum
+    scal_matrix(momentum, l.dw);
     // l.db *= momentum
+    scal_matrix(momentum, l.db);
 
 }
 
